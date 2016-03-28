0810ae5ae2b13b85a8dfb09be5d5262decdf19ea
adamShaleen <adamshaleen@gmail.com>
2016-03-27 22:36:59 -0600

    3.27.16

--- practice.js
+++ practice.js
@@ -1,38 +1,55 @@
 //Once you complete a problem, open up Chrome and check the answer in the console.
 
 
-//Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
+//Create an object called me. Give it a key of name with the value being your name,
+//and another key of age with the value being your age. Then alert your name using dot notation.
 
   //Code here
 
+  var me = {
+      name: 'Adam',
+      age: 32
+  }
 
-
+alert(me.name);
 
 //NEXT PROBLEM
 
 
-
-
-//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
-
-  //Code here
-
-
-//After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
+//Make a 'favoriteThings' object that contains the following keys: band, food,
+//person, book, movie, holiday. Have the values to those keys be your favorite
+//thing in that category.
 
   //Code here
 
+   var favoriteThings = {
+       band: 'Thrice',
+       food: 'tacos',
+       person: 'wifu',
+       book: 'Average American Male',
+       movie: 'LOTR',
+       holiday: 'birthmas'
+   };
 
-//Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'.
+//After you've made your object, add another key named 'car' with the value
+//being your favorite car and then another key named 'brand' with the value being
+//your favorite brand.
 
   //Code here
 
+   favoriteThings.car = 'Subaru';
+   favoriteThings.brand = 'deluxe';
 
+//Now change the food key in your favoriteThings object to be 'Lettuce' and
+//change the book key in your favoriteThings object to be '50 Shades of Gray'.
+
+  //Code here
+
+   favoriteThings.food = 'Lettuce';
+   favoriteThings.book = '50 Shades of Gray';
 
 
 //NEXT PROBLEM
-
-
 
 /*Create an empty Object called backPack. Now, create a variable called 'item'
 and set it equal to the string 'firstPocket'. Using bracket notation,
@@ -44,9 +61,15 @@ that is named color, with the value being the color of your backpack. */
 
   //Code here
 
+  var backPack = {};
+  var item = 'firstPocket';
+  backPack['firstPocket'] = 'chapstick';
+
 //After you do the above, alert your entire backPack object.
 
   //Code here
+
+  alert(backPack);
 
 /*You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
@@ -54,60 +77,88 @@ Instead, console.log your whole backPack object and then check out the console. 
 
   //Code here
 
-
+  console.log(backPack);
 
 
 //NEXT PROBLEM
 
 
-
-
-//Create an 'alsoMe' object with the following properties name, age, height, gender, married, eyeColor, hairColor. Fill those properties in with the appropriate values.
+//Create an 'alsoMe' object with the following properties name, age, height,
+//gender, married, eyeColor, hairColor. Fill those properties in with the
+//appropriate values.
 
   //Code Here
+
+  var alsoMe = {
+      name: 'Adam',
+      age: 32,
+      height: 5.9,
+      gender: 'male',
+      married: 'yes',
+      eyeColor: 'blue',
+      hairColor: 'black'
+  };
 
 //Now, loop through your object and alert every value. *Tyler --> 24 --> 6'0 --> Male, etc etc
 
   //Code Here
 
-
-
+  for (var prop in alsoMe) {   //just lists undefined...?
+      alert(alsoMe.prop);
+  }
 
 //NEXT PROBLEM
 
-
-
-
-//Create an Object called 'album' with 5 keys named different song titles that you make up, with the values being the length of each song.
+//Create an Object called 'album' with 5 keys named different song titles that
+//you make up, with the values being the length of each song.
 
   //Code Here
+
+  var album = {
+      song1: 100,
+      song2: 200,
+      song3: 300,
+      song4: 400,
+      song5: 500
+  };
 
 //Now, loop through your album object alerting every song title individually.
 
   //Code Here
 
-
-
-
-//NEXT PROBLEM
-
-
-
-
-//Create an object called states that has 5 US states as properties with the values being their population (doesn't have to be accurate).
-
-  //Code Here
-
-//Now, loop through your states object and if the states population is greater than 30K, alert that state.
-
-  //Code Here
-
-
+  for (var prop in album) {
+      alert(album.prop);
+  }
 
 
 //NEXT PROBLEM
 
 
+//Create an object called states that has 5 US states as properties with the
+//values being their population (doesn't have to be accurate).
+
+  //Code Here
+
+  var states = {
+      Minnesota: 45,
+      Utah: 200000,
+      California: 65,
+      Florida: 999999,  // all insane
+      Maine: 45
+  };
+
+//Now, loop through your states object and if the states population is
+//greater than 30K, alert that state.
+
+  //Code Here
+
+  for (var prop in states) {
+      if (states.prop > 30000) {
+          alert(states.prop);
+      }
+  }
+
+//NEXT PROBLEM
 
 
 var user1 = {
@@ -123,17 +174,19 @@ that each value is truthy. If it's not truthy, remove it from the object. */
 
   //Code Here
 
-//Once you get your truthy Object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
+  for (var prop in user1) {
+      if (user1.prop !== true) {
+          delete user1.prop;
+      }
+  }
+
+//Once you get your truthy Object, Change the remaining values in the object to
+//be specific to you (name: 'your name', username: 'your username'), rather
+//than my information.
 
   //Code Here
 
-
-
-
 //NEXT PROBLEM
-
-
-
 
 var user2 = {
         name: 'Tyler McGinnis',
